ffbf7caa6d5fa9eee8900b1c640db8d17b3c870e
shokh001 <shohjahondilmurodov56@gmail.com>
2022-04-15 00:13:03 +0300

    changed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import GroupWorkIcon from '@material-ui/icons/GroupWork';
-import VideocamIcon from '@material-ui/icons/Videocam';
 import './header.css'
+import { Container } from '@material-ui/core'
 
 const Header = () => {
     return (
-        <span onClick={() => window.scroll(0, 0)} className='header'>
-            <GroupWorkIcon className='icon' />
-            Movie React App
-            <VideocamIcon className='icon' />
-        </span>
+        <Container className='warpper'>
+            <div className='header'>
+                <span onClick={() => window.scroll(0, 0)} >The Movie</span>
+                <GroupWorkIcon className='icon' />
+            </div>
+        </Container>
     )
 }
 
